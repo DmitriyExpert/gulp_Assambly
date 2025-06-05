31dbc9035825c1f0da73de46fc737c3e114fbdf8
Dmitriy <ddpemvr@mail.ru>
2025-06-05 17:25:55 +0500

    adding fonts function in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,12 @@ function css() {
         .pipe(browserSync.reload({stream: true}));
 }
 
+function fonts() {
+  return gulp.src('src/fonts/**/*.{woff2,woff,otf,ttf,eot}')
+          .pipe(gulp.dest('dist/fonts'))
+          .pipe(browserSync.reload({stream: true}))
+}
+
 function images() {
   return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
             .pipe(gulp.dest('dist/images'))
@@ -47,18 +53,20 @@ function clean() {
   return del('dist');
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, fonts));
 
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/blocks/**/*.css'], css);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/fonts/**/*.{woff2, woff, otf, ttf, eot}'], fonts);
 }
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.watchapp = watchapp; 
 exports.build = build; 
 exports.clean = clean;
+exports.fonts = fonts;
 exports.images = images;
 exports.css = css; 
 exports.html = html;
